50d2e0d8548f8398ff564fa2fb7580cbfc2b24b9
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-03 18:21:42 +0530

    removed all css and added tailwindcss to proj

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { restaurantList } from "../config";
+import restaurantList from "../config";
 import RestaurantCard from "./ResturantCard";
 import ShimmerUI  from "./ShimmerUI"; 
 import {Link} from "react-router-dom";
@@ -25,7 +25,7 @@ const Body = () =>{
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.9205207&lng=70.3777302&page_type=DESKTOP_WEB_LISTING');
         const jsonData = await data.json();
         // console.log(jsonData);
-        // console.log(jsonData?.data?.cards[2]?.data?.data?.cards);
+        // console.log(jsonData?.data?.cards[0]?.data?.data?.cards);
         setAllRestaurants(jsonData?.data?.cards[2]?.data?.data?.cards);
         setFilteredRestaurants(jsonData?.data?.cards[2]?.data?.data?.cards);
     }
--- src/utils/useRestaurantList.js
+++ src/utils/useRestaurantList.js
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const useRestaurantList = () => {
-    const [filteredRestaurants,setFilteredRestaurants] = useState([]);
     const [allRestaurants,setAllRestaurants] = useState([]);
     useEffect(()=>{
         getRestraunt();
@@ -11,8 +10,10 @@ const useRestaurantList = () => {
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.9205207&lng=70.3777302&page_type=DESKTOP_WEB_LISTING');
         const jsonData = await data.json();
         // console.log(jsonData);
-        // console.log(jsonData?.data?.cards[2]?.data?.data?.cards);
-        setAllRestaurants(jsonData?.data?.cards[2]?.data?.data?.cards);
-        setFilteredRestaurants(jsonData?.data?.cards[2]?.data?.data?.cards);
+        // console.log(jsonData?.data?.cards[0]?.data?.data?.cards);
+        setAllRestaurants(jsonData?.data?.cards[0]?.data?.data?.cards);
     }
+    return allRestaurants;
 }
+
+export default useRestaurantList;
